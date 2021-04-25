a7380cb06781d737d60f89a3f5c019716e8c598e
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-04-25 09:33:16 +0200

    feat: Add class style to links

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 console.log("Js has been added");
 /**
+ * Define Global Variables
+ * 
+*/
+
+//get access to nav ul to add li later
+const navContainer= document.querySelector("#navbar__list");
+//get access to all sections in the page, we'll add li accordingly 
+const sections=document.querySelectorAll("section");
+//create fragment to improve Reflow time consuming
+let liFragment=document.createDocumentFragment();
+/**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
 
-
+// add class style to links in the nav bar (li and href link to <a>)
+function addLinkClassStyles(li,section){
+    li.classList.add("menu__link");
+    li.firstChild.setAttribute("href",`#${section.id}`);
+}
 /**
  * End Helper Functions
  * Begin Main Functions
