9551afa3954302edd6504b0c957ed6d5c6c9701b
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-04-25 09:38:56 +0200

    feat: Add navbar links navigate to each section accordingly

--- js/app.js
+++ js/app.js
@@ -10,13 +10,34 @@ const navContainer= document.querySelector("#navbar__list");
 const sections=document.querySelectorAll("section");
 //create fragment to improve Reflow time consuming
 let liFragment=document.createDocumentFragment();
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-
+// initialize links of the nav bar 
+function initNavLinks (){
+    //1. loop the sections and create li & <a> accordingly
+    for(section of sections){
+        //2. create li elements
+        const li=document.createElement("li");
+        //3.create a element
+        const a=document.createElement("a");
+        //4. set the name of the link
+        a.textContent=section.getAttribute("data-nav");
+        console.log(a.textContent);      //test
+        //5. add a to li
+        li.appendChild(a);
+        //6. add li to fragment
+        liFragment.appendChild(li);
+        //7. add li style class
+        addLinkClassStyles(li,section);
+    }
+    //8. add fragment to navContainer ul (that decreases Reflow)
+    navContainer.appendChild(liFragment);
+}
 // add class style to links in the nav bar (li and href link to <a>)
 function addLinkClassStyles(li,section){
     li.classList.add("menu__link");
@@ -49,4 +70,4 @@ function addLinkClassStyles(li,section){
 
 // Set sections as active
 
-
+initNavLinks();
