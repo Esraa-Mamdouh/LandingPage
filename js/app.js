1ddddb2b32199dd5606369a48f2bba1c8714f139
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-04-25 09:23:07 +0200

    feat: adding new sections

--- js/app.js
+++ js/app.js
@@ -1,24 +1,4 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
+console.log("Js has been added");
 /**
  * End Global Variables
  * Start Helper Functions
